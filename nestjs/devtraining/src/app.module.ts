93dbb7a0ccbd674eac9429fa0925f7611816099d
kevin <kevindiegodasilvasousa@gmail.com>
2023-08-08 11:40:10 -0300

    chore: Criado modulo course

--- nestjs/devtraining/src/app.module.ts
+++ nestjs/devtraining/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoursesController } from './courses/courses.controller';
-import { UsersController } from './users/users.controller';
+import { CoursesModule } from './courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoursesController, UsersController],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5032,
+      username: 'postgres',
+      password: 'docker',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- nestjs/devtraining/src/courses/courses.controller.ts
+++ nestjs/devtraining/src/courses/courses.controller.ts
@@ -1,9 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CourseService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly courseService: CourseService) {}
+
   @Get()
   findAll() {
-    return 'Listagem de cursos';
+    return this.courseService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.courseService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCouseDto: CreateCourseDto) {
+    return this.courseService.create(createCouseDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.courseService.update(id, updateCourseDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.courseService.remove(id);
   }
 }
