78fabdf3fb066f115da5a69973345d473350f26d
kevin <kevindiegodasilvasousa@gmail.com>
2023-08-22 10:41:40 -0300

    fix: removendo pasta sem uso

--- nestjs/api-prisma/src/users/users.service.ts
+++ nestjs/api-prisma/src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
 import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
@@ -12,11 +14,18 @@ export class UsersService {
   }
 
   findAll() {
+    //throw new UnauthorizedError('Nao autorizado.');
     return this.repository.findAll();
   }
 
-  findOne(id: number) {
-    return this.repository.findOne(id);
+  async findOne(id: number): Promise<UserEntity> {
+    const user = await this.repository.findOne(id);
+
+    if (!user) {
+      throw new NotFoundError('Usuario nao encontrado.');
+    }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
